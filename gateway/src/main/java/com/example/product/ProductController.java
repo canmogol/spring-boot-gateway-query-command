dca79c54fb24149d25b38c05856ff34ec6b8dc6c
Can MOGOL <canmogol@gmail.com>
2024-10-21 22:46:09 +0300

    Cosmetic updates.

--- gateway/src/main/java/com/example/product/ProductController.java
+++ gateway/src/main/java/com/example/product/ProductController.java
@@ -6,7 +6,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
--- gateway/src/main/java/com/example/user/UserValidator.java
+++ gateway/src/main/java/com/example/user/UserValidator.java
@@ -8,6 +8,10 @@ import java.util.Objects;
 @Component
 public class UserValidator {
 
+    private static final int MIN_USERNAME_LENGTH = 4;
+    private static final int MAX_LENGTH = 20;
+    private static final int MIN_NAME_LENGTH = 8;
+
     public void validateUser(UserDTO userDTO) throws InvalidUserException {
         if (Objects.isNull(userDTO)) {
             throw new InvalidUserException("User cannot be null");
@@ -21,11 +25,11 @@ public class UserValidator {
             throw new InvalidUserException("Name cannot be null");
         }
         // username should be at least 4 characters and maximum 20 characters
-        if (userDTO.username().length() < 4 || userDTO.username().length() > 20) {
+        if (userDTO.username().length() < MIN_USERNAME_LENGTH || userDTO.username().length() > MAX_LENGTH) {
             throw new InvalidUserException("Username must be between 4 and 20 characters long");
         }
         // id should be at least 8 characters and maximum 20 characters
-        if (userDTO.name().length() < 8 || userDTO.name().length() > 20) {
+        if (userDTO.name().length() < MIN_NAME_LENGTH || userDTO.name().length() > MAX_LENGTH) {
             throw new InvalidUserException("Name must be between 8 and 20 characters long");
         }
     }
