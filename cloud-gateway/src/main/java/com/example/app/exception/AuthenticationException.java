8f8482fcb18ad3200424bb9d03c749e930f56885
Can MOGOL <canmogol@gmail.com>
2024-10-25 00:54:31 +0300

    Remove unused classes.
    - Use /product prefix

--- cloud-gateway/src/main/java/com/example/app/exception/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.exception;
-
-public class AuthenticationException extends Exception {
-
-    public AuthenticationException(final String message) {
-        super(message);
-    }
-
-}
